389bf7bcb656860d8eb518da2b2e510b50c01d07
Nicolas <nikolasdurango@gmail.com>
2023-06-26 01:33:39 -0500

    created Book model, and updatep Author model

--- apps/book/models.py
+++ apps/book/models.py
@@ -7,6 +7,7 @@ class Author(models.Model):
     last_name = models.CharField(max_length=150, blank=False, null=False)
     nationality = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
+    created_date = models.DateField(('Created date'), auto_now=True, auto_now_add=False)
     
     class Meta:
         verbose_name = 'Author'
@@ -15,3 +16,16 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, blank=False, null=False)
+    publish_date = models.DateField('Publication date', blank=False, null=False)
+    author_id = models.ManyToManyField(Author)
+    created_date = models.DateField(('Created date'), auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return self.title
