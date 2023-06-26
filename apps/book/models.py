d38d517344022b5fd0ea5db4c993c06c390218ac
Nicolas <nikolasdurango@gmail.com>
2023-06-26 00:59:40 -0500

    create Author  model

--- apps/book/models.py
+++ apps/book/models.py
@@ -6,5 +6,12 @@ class Author(models.Model):
     name = models.CharField(max_length=150, blank=False, null=False)
     last_name = models.CharField(max_length=150, blank=False, null=False)
     nationality = models.CharField(max_length=50, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
     
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
